7ce8a75bc59f635822c08ba621a77b9aac0999b4
Gianluca Paschoalotti <gianluca170703@gmail.com>
2024-08-20 17:05:36 -0300

    Added Search bar component

---
+++ src/app/components/SearchBar/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import SearchIcon from "@mui/icons-material/Search";
+
+const SearchBar: React.FC = () => {
+  return (
+    <TextField
+      variant="outlined"
+      placeholder="Para onde?"
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon style={{ color: "#F8A801", width: "28px" }} />
+          </InputAdornment>
+        ),
+        style: {
+          backgroundColor: "#1c1c1c",
+          color: "white",
+          borderRadius: 30,
+        },
+      }}
+      className="w-80"
+    />
+  );
+};
+
+export default SearchBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import FullScreenMap from '../app/components/Map/index'
+import FullScreenMap from "../app/components/Map/index";
+import SearchBar from "../app/components/SearchBar/index";
 
 const Home: React.FC = () => {
   return (
     <div>
+      <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 z-50 w-80 mb-6">
+        <SearchBar />
+      </div>
       <FullScreenMap />
     </div>
   );
