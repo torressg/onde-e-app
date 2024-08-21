794ecc09eab1051b181d91a1ef5e85b4f6bf98cd
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2024-08-21 12:43:09 -0300

    create Ambientes page

---
+++ src/app/Ambientes/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AmbientePage: React.FC = () => {
+    return (
+        <div className="relative">
+            <h1>Ambientes</h1>
+        </div>
+    );
+};
+
+export default AmbientePage;
