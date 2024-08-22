b88912033f9edd02d3337d12c4d3e4eeec0b17be
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2024-08-22 12:05:28 -0300

    fix localStorageService to better arch

--- src/services/LocalStorageService.ts
+++ src/services/LocalStorageService.ts
@@ -1,13 +1,37 @@
 export const LocalStorageService = {
   setRecentDestination: function (Destination: any) {
-    localStorage.setItem("RecentDestination", Destination);
+    // Recupera a lista atual do localStorage
+    const recentDestinations = this.getRecentDestinations();
+
+    // Remove a duplicata, se existir, para evitar itens repetidos
+    const filteredDestinations = recentDestinations.filter(
+      (item: any) => item !== Destination
+    );
+
+    // Adiciona o novo destino ao início da lista
+    filteredDestinations.unshift(Destination);
+
+    // Limita a lista a no máximo 4 itens
+    const limitedDestinations = filteredDestinations.slice(0, 4);
+
+    // Armazena a lista atualizada no localStorage
+    localStorage.setItem(
+      "RecentDestinations",
+      JSON.stringify(limitedDestinations)
+    );
   },
 
-  getRecentDestination: function () {
-    return localStorage.getItem("RecentDestination");
+  getRecentDestinations: function () {
+    // Recupera a lista de destinos
+    const serializedDestinations = localStorage.getItem("RecentDestinations");
+    if (serializedDestinations) {
+      return JSON.parse(serializedDestinations);
+    }
+    return []; // Retorna uma lista vazia se não houver nada no localStorage
   },
 
-  deleteSession: function () {
-    localStorage.removeItem("RecentDestination")
+  deleteRecentDestinations: function () {
+    // Remove a lista de destinos
+    localStorage.removeItem("RecentDestinations");
   },
 };
