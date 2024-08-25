bfeb861cc09c907dc237adc5d6a7ff01fd5d3a48
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2024-08-25 09:27:03 -0300

    feat: limiting zooms

--- src/app/components/Map/index.tsx
+++ src/app/components/Map/index.tsx
@@ -12,7 +12,9 @@ const FullScreenMap = () => {
             container: 'map',
             style: 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
             center: [-46.57924457368841,-23.618155335655388], // Substitua pelas coordenadas específicas-23.618155335655388, -46.57924457368841
-            zoom: 15, // Ajuste o zoom para iniciar focado
+            zoom: 12, // Ajuste o zoom para iniciar focado
+            minZoom: 19,
+            maxZoom: 20,
             // bearing: 30, // Ângulo de rotação para alinhar o corredor
             pitch: 20, // Inclinação para uma visão mais tridimensional
         });
