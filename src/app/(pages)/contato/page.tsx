9a874fd64d36c7b9c6771365deef4df9066a8f08
Gianluca Paschoalotti <gianluca170703@gmail.com>
2024-08-28 12:58:30 -0300

    Improve: stylization of the “Contato” page

--- src/app/(pages)/contato/page.tsx
+++ src/app/(pages)/contato/page.tsx
@@ -4,12 +4,16 @@ import React from "react";
 const Contato = () => {
   return (
     <>
-      <div>
-        <header>
-          <div className="fixed top-4 left-4 z-50">
+      <div className=" bg-ambientBG min-h-screen">
+        <header className="pt-4">
+          <div className="fixed ml-4 z-50">
             <BurgerMenu />
           </div>
+          <div className="mt-16 px-4 pb-4 inline-flex font-bold justify-between w-full">
+            <h2 className="text-2xl">Contato</h2>
+          </div>
         </header>
+        <hr className="text-laranja py-5" />
       </div>
     </>
   );
