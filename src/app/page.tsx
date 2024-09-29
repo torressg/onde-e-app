7b610f53dd8106113895e11ac6cedca6b2025d74
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2024-09-28 22:21:28 -0300

    recent menu text to search input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ const Home: React.FC = () => {
           <BurgerMenu />
         </div>
         <div className="fixed top-4 right-4 z-40">
-          <RecentMenu inputValue={inputValue} />
+          <RecentMenu inputValue={inputValue} setInputValue={setInputValue} />
         </div>
       </header>
 
